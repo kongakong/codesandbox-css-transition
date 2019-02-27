85bb25335ec5f0d5132e50713deaeb9279187af0
Anthony Kong <kongakong@users.noreply.github.com>
2019-02-27 18:55:56 +1100

    Apply mobx functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import mobx, { observable, action, decorate } from "mobx";
+import mobxReact from "mobx-react";
 
 import "./styles.css";
 
+const Main = mobxReact.observer(
+  class Main extends React.Component {
+    // @observable internalState = false;
+    constructor(props) {
+      super(props);
+      this.internalState = false;
+    }
+
+    toggleMenu = () => {
+      console.log("Toggle button");
+      this.internalState = !this.internalState;
+    };
+
+    render() {
+      return (
+        <div className="App">
+          <h1>Toggle State</h1>
+          <input type="button" onClick={this.toggleMenu} value="Toggle" />
+          <div style={{ paddingTop: "25px" }}>
+            internalState {this.internalState.toString()}{" "}
+          </div>
+        </div>
+      );
+    }
+  }
+);
+
+decorate(Main, {
+  internalState: observable,
+  toggleMenu: action
+});
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <Main />;
 }
 
 const rootElement = document.getElementById("root");
